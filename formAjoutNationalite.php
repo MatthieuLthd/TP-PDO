0d642f26a8f5ab79463d5537c2b1e5556594c4ac
MatthieuLthd <matt.leauthaud@gmail.com>
2023-02-08 17:15:20 +0100

    formulaire nationalite 7:27

--- formAjoutNationalite.php
+++ formAjoutNationalite.php
@@ -10,7 +10,7 @@
             <input type="text" class='form-control' id="libelle" placehoder="Saisir le libellé" name="libelle">
         </div>
         <div class="row">
-            <div class="col"> <a href="listeNationalites.php" class="btn btn-warning">Revenir 4 la liste</a> </div>
+            <div class="col"> <a href="listeNationalites.php" class="btn btn-warning">Revenir à la liste</a> </div>
             <div class="col"><button type="submit" class="btn btn-success"> Ajouter </button> </div>
         </div>
     </form>
